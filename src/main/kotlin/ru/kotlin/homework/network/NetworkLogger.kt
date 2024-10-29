898f42b0c8cc95b6b382467810339b3661ce8a9f
IgorAlexeyv <IgorAlexeyv@gmail.ru>
2024-10-29 20:54:49 +0300

    dz1+dz2

--- src/main/kotlin/ru/kotlin/homework/network/NetworkLogger.kt
+++ src/main/kotlin/ru/kotlin/homework/network/NetworkLogger.kt
@@ -15,9 +15,9 @@ sealed class ApiException(message: String) : Throwable(message) {
     data object UnknownException: ApiException("Unknown exception")
 }
 
-class ErrorLogger<E : Throwable> {
+class ErrorLogger<in E : Throwable> {
 
-    val errors = mutableListOf<Pair<LocalDateTime, E>>()
+    private val errors = mutableListOf<Pair<LocalDateTime, *>>()
 
     fun log(response: NetworkResponse<*, E>) {
         if (response is Failure) {
@@ -27,7 +27,7 @@ class ErrorLogger<E : Throwable> {
 
     fun dumpLog() {
         errors.forEach { (date, error) ->
-            println("Error at $date: ${error.message}")
+            println("Error at $date: ${error.toString()}")
         }
     }
 }
