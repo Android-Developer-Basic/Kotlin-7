49d5fe109669570c5629e9cd257b70dd9a52fd5c
SpiritualAdviser <pomaxam@narod.ru>
2024-10-31 23:10:49 +0300

    my decision

--- src/main/kotlin/ru/kotlin/homework/network/NetworkLogger.kt
+++ src/main/kotlin/ru/kotlin/homework/network/NetworkLogger.kt
@@ -10,29 +10,42 @@ import java.time.LocalDateTime
  * Известный вам список ошибок
  */
 sealed class ApiException(message: String) : Throwable(message) {
-    data object NotAuthorized : ApiException("Not authorized")
-    data object NetworkException : ApiException("Not connected")
-    data object UnknownException: ApiException("Unknown exception")
+    data object NotAuthorized : ApiException("Not authorized") {
+        private fun readResolve(): Any = NotAuthorized
+    }
+
+    data object NetworkException : ApiException("Not connected") {
+        private fun readResolve(): Any = NetworkException
+    }
+
+    data object UnknownException : ApiException("Unknown exception") {
+        private fun readResolve(): Any = UnknownException
+    }
 }
 
 class ErrorLogger<E : Throwable> {
 
-    val errors = mutableListOf<Pair<LocalDateTime, E>>()
+    private val errors = mutableListOf<Pair<LocalDateTime, *>>()
 
-    fun log(response: NetworkResponse<*, E>) {
+    fun log(response: NetworkResponse<*, *>) {
         if (response is Failure) {
             errors.add(response.responseDateTime to response.error)
         }
     }
 
+    fun dump(): MutableList<Pair<LocalDateTime, *>> {
+        return errors
+    }
+
     fun dumpLog() {
         errors.forEach { (date, error) ->
-            println("Error at $date: ${error.message}")
+            println("Error at $date: $error")
         }
     }
 }
 
 fun processThrowables(logger: ErrorLogger<Throwable>) {
+
     logger.log(Success("Success"))
     Thread.sleep(100)
     logger.log(Success(Circle))
@@ -42,12 +55,17 @@ fun processThrowables(logger: ErrorLogger<Throwable>) {
     logger.dumpLog()
 }
 
-fun processApiErrors(apiExceptionLogger: ErrorLogger<ApiException>) {
+fun processApiErrors(apiExceptionLogger: ErrorLogger<*>) {
     apiExceptionLogger.log(Success("Success"))
     Thread.sleep(100)
     apiExceptionLogger.log(Success(Circle))
     Thread.sleep(100)
-    apiExceptionLogger.log(Failure(ApiException.NetworkException))
+
+    val api = ApiException.NetworkException
+    val tt = Failure(api)
+
+
+    apiExceptionLogger.log(tt)
 
     apiExceptionLogger.dumpLog()
 }
@@ -55,10 +73,16 @@ fun processApiErrors(apiExceptionLogger: ErrorLogger<ApiException>) {
 fun main() {
     val logger = ErrorLogger<Throwable>()
 
+
     println("Processing Throwable:")
     processThrowables(logger)
 
     println("Processing Api:")
     processApiErrors(logger)
+
+    val errors = logger.dump()
+    errors.forEach {
+        println("List Errors = $it")
+    }
 }
 
